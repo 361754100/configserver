115e04cf06e0fef2d56ff81d826d7cf94ec556d9
Mojianzhang <361754100@qq.com>
2018-05-10 15:50:46 +0800

    增加可用的测试用例

--- src/main/java/com/hro/core/configserver/ConfigserverApplication.java
+++ src/main/java/com/hro/core/configserver/ConfigserverApplication.java
@@ -1,11 +1,14 @@
 package com.hro.core.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ConfigserverApplication {
 
     public static void main(String[] args) {
